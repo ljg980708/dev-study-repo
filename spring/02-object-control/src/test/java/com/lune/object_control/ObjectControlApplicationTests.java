a56264fc3b042397265059cba8570b39271468b9
ljg980708 <ljg980708@gmail.com>
2024-06-18 08:45:37 +0900

    24-06-18 spring component

---
+++ spring/02-object-control/src/test/java/com/lune/object_control/ObjectControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lune.object_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
