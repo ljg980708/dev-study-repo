6beff255270959dafccf6f70adf30263b27e9578
ljg980708 <ljg980708@gmail.com>
2024-06-19 06:06:09 +0900

    24-06-19 spring advanced features

---
+++ spring/03-advanced-features/src/test/java/com/lune/advanced_features/AdvancedFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lune.advanced_features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
