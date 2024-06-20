ed2ac4a65853ea739d6a25c5d5ec407ae42b4c2c
ljg980708 <ljg980708@gmail.com>
2024-06-20 21:40:32 +0900

    24-06-20 lazy initialization

---
+++ spring/03-advanced-features/src/main/java/com/lune/lazy/LazyInitializationExample.java
@@ -0,0 +1,10 @@
+package com.lune.lazy;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class LazyInitializationExample {
+
+}
