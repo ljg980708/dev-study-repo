b917b990d9841af4975635ae05acea36f6ae7621
ljg980708 <ljg980708@gmail.com>
2024-06-20 21:43:06 +0900

    24-06-20 spring boot begin

---
+++ spring/04-spring-boot-begin/src/test/java/com/lune/spring_boot_begin/SpringBootBeginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lune.spring_boot_begin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBeginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
