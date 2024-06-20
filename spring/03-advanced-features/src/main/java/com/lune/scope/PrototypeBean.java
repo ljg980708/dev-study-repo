ce2a13d32f96e6095dce16b37b788eff6cf52904
ljg980708 <ljg980708@gmail.com>
2024-06-20 21:40:44 +0900

    24-06-20 scope

---
+++ spring/03-advanced-features/src/main/java/com/lune/scope/PrototypeBean.java
@@ -0,0 +1,10 @@
+package com.lune.scope;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Scope("prototype")
+@Component
+public class PrototypeBean {
+
+}
